559a997161f6d9cdbaa9fd43279d720924c8db29
victor-chukwu <okenwa1993@gmail.com>
2022-03-17 21:17:06 +0100

    Add controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,27 +4,58 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
   end
+  # def index
+  #   @comments = Post.comments
+
+  #   i = 0
+  #   @commentt = []
+  #   while i < @comments.length
+  #    comment = {}
+  #    comment['comment'] = @comments[i]
+  #    comment['post'] = Post.find_by(id: @comments.post)
+  #    comment['user'] = User.find_by(id: @comments[i].user)
+  #     @commentt.push(comment)
+  #     i += 1
+  #   end
+  #   # @commentt['post'] =comment.find_by(id: @comment.post)
+  #   render json: @commentt, only: [:description, :user_id]
+  # end
 
   # GET /comments/1
   # GET /comments/1.json
-  def show; end
+  def show
+    @commentt = {}
+    @commentt['comment'] = @comment
+    @commentt['user'] = User.find_by(id: @comment.user)
+    @commentt['post'] = Post.find_by(id: @comment.post)
+    # @commentt
+    render json: @commentt
+  end
 
-  # POST /comments
-  # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Post.comments.new
+    @comment = User.comments.new
+    @comment = params[:comment][:description, :user_id]
+    @comment.user = User.find_by(id: params[:comment][:user])
+    @comment.post = Post.find_by(id: params[:comment][:post])
+    # @post.doctor = Doctor.find_by(id: params[:post][:doctor])
+    # @post_hash = Post.return_post_user(@post)
+
+    @commentt = {}
+    @commentt['comment'] = @comment
+    @commentt['user'] = User.find_by(id: @comment.user)
+    @commentt['post'] = Post.find_by(id: @comment.post)
 
     if @comment.save
-      render :show, status: :created, location: @comment
+      @comment.post.commentscounter += 1
+      puts('hklal comment')
+      render json: @commentt, status: :created, location: @comments
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     if @comment.update(comment_params)
       render :show, status: :ok, location: @comment
@@ -33,21 +64,18 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = Comment.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:describtion, :User_id, :Post_id)
+    params.require(:comment).permit(:description, :user_id)
   end
 end
