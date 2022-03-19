40ee545e7b7892b7edefbb266f9b37619423d088
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-03-19 02:36:56 +0100

    add condition if logged in successful

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create]
   end
   resources :users
+  resources :sessions, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
