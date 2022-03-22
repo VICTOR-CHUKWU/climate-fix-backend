422fa7d888fa1c6179213104933d6f1a48369b51
victor-chukwu <okenwa1993@gmail.com>
2022-03-22 20:47:18 +0100

    fixing linters

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,13 +1,11 @@
-module  CurrentUserConcern
+module CurrentUserConcern
   extend ActiveSupport::Concern
 
   included do
     before_action :set_current_user
   end
-  
+
   def set_current_user
-    if session[:user_id]
-      @current_user= User.find(session[:use_id])
-    end
+    @current_user = User.find(session[:use_id]) if session[:user_id]
   end
 end
